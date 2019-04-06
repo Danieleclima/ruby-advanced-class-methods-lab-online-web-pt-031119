c1d06065b35e53426046524cf2ebdceeb5b3e02a
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-04-06 19:46:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -55,10 +55,10 @@ end
 def Song.new_from_filename (file)
   lenght = file.size
   file [lenght -4, lenght] = ""
-  array = file.split("-")
+  array = file.split(" - ")
   song = self.new
   song.name = array[1]
-  song.artist_name = array[0].delete(" ")
+  song.artist_name = array[0]
   song
 end
 
